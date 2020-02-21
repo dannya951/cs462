8a9c5addf845ebd9bbe1f432cf13301d954eff17
dannya951 <dannya951@users.noreply.github.com>
2020-02-21 05:59:16 -0700

    Update spa.js

--- lab_5/spa.js
+++ lab_5/spa.js
@@ -60,21 +60,5 @@ angular.module('spa', [])
     $scope.toggle_page = function() {
       $scope.temperatures_page = !($scope.temperatures_page);
     };
- 
-    /*
-    $scope.timeDiff = function(timing) {
-      var bgn_sec = Math.round(Date.parse(timing.time_out)/1000);
-      var end_sec = Math.round(Date.parse(timing.time_in)/1000);
-      var sec_num = end_sec - bgn_sec;
-      var hours   = Math.floor(sec_num / 3600);
-      var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-      var seconds = sec_num - (hours * 3600) - (minutes * 60);
-   
-      if (hours   < 10) {hours   = "0"+hours;}
-      if (minutes < 10) {minutes = "0"+minutes;}
-      if (seconds < 10) {seconds = "0"+seconds;}
-      return hours+':'+minutes+':'+seconds;
-    }
-    */
   }
 ]);
