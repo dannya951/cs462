f23285f95c5cf7fe997ec2068ae376405f5e587c
dannya951 <dannya951@users.noreply.github.com>
2020-02-20 23:14:10 -0700

    Update spa.js

--- lab_5/spa.js
+++ lab_5/spa.js
@@ -1,4 +1,4 @@
-angular.module('timing', [])
+angular.module('spa', [])
 .directive('focusInput', function($timeout) {
   return {
     link: function(scope, element, attrs) {
@@ -13,9 +13,14 @@ angular.module('timing', [])
 .controller('MainCtrl', [
   '$scope','$http','$window',
   function($scope,$http,$window){
-    $scope.timings = [];
+
+    //$scope.timings = [];
+    $scope.current_temperature = "No Reading Yet";
+    $scope.temperatures = [];
+    $scope.threshold_violations = [];
     $scope.eci = $window.location.search.substring(1);
- 
+
+    /*
     var bURL = 'http://localhost:8080/sky/event/'+$scope.eci+'/eid/timing/started';
     $scope.addTiming = function() {
       var pURL = bURL + "?number=" + $scope.number + "&name=" + $scope.name;
@@ -25,7 +30,9 @@ angular.module('timing', [])
         $scope.name='';
       });
     };
+    */
  
+    /*
     var iURL = 'http://localhost:8080/sky/event/'+$scope.eci+'/eid/timing/finished';
     $scope.finished = function(number) {
       var pURL = iURL + "?number=" + number;
@@ -33,7 +40,9 @@ angular.module('timing', [])
         $scope.getAll();
       });
     };
+    */
  
+    /*
     var gURL = 'http://localhost:8080/sky/cloud/'+$scope.eci+'/timing_tracker/entries';
     $scope.getAll = function() {
       return $http.get(gURL).success(function(data){
@@ -42,7 +51,35 @@ angular.module('timing', [])
     };
  
     $scope.getAll();
+    */
+    var ctURL = 'http://localhost:8080/sky/cloud/'+$scope.eci+'/temperature_store/current_temperature';
+    $scope.get_current_temperature = function () {
+      return $http.get(ctURL).success(function(data){
+        angular.copy(data, $scope.current_temperature);
+      });
+    };
+
+    $scope.get_current_temperature();
+
+    var tURL = 'http://localhost:8080/sky/cloud/'+$scope.eci+'/temperature_store/temperatures';
+    $scope.get_temperatures = function() {
+      return $http.get(tURL).success(function(data) {
+        angular.copy(data, $scope.temperatures);
+      });
+    };
+
+    $scope.get_temperatures();
+
+    var tvURL = 'http://localhost:8080/sky/cloud/'+$scope.eci+'/temperature_store/threshold_violations';
+    $scope.get_threshold_violations() {
+      return $http.get(tvURL).success(function(data){
+        angular.copy(data, $scope.threshold_violations);
+      });
+    };
+
+    $scope.get_threshold_violations();
  
+    /*
     $scope.timeDiff = function(timing) {
       var bgn_sec = Math.round(Date.parse(timing.time_out)/1000);
       var end_sec = Math.round(Date.parse(timing.time_in)/1000);
@@ -56,5 +93,6 @@ angular.module('timing', [])
       if (seconds < 10) {seconds = "0"+seconds;}
       return hours+':'+minutes+':'+seconds;
     }
+    */
   }
 ]);
